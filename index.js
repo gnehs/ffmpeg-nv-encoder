325243670cbc53288225e3f6aad18ed692192bc2
gnehs <jayuiop@gmail.com>
2020-10-02 21:03:39 +0800

    removed progress, disabled decoder

--- index.js
+++ index.js
@@ -12,16 +12,18 @@ if (!fs.existsSync(outputDir)) {
 // encoder & decoder
 let list2console = list => list.map((x, i) => (`${i + 1}. ${x}`)).join('\n')
 let encoderList = ['hevc_nvenc', 'h264_nvenc']
+/*
 let decoderList = ['h264_cuvid', 'hevc_cuvid', 'mpeg4_cuvid', 'vc1_cuvid', 'vp8_cuvid', 'vp9_cuvid', 'h263_cuvid', 'mjpeg_cuvid', 'mpeg1_cuvid', 'mpeg2_cuvid', 'cpu']
 console.log(`===\nDecoder list:\n${list2console(decoderList)}`);
 let decoder = decoderList[parseInt(prompt('Choose decoder: ')) - 1] || '';
+*/
 console.log(`===\nEncoder list:\n${list2console(encoderList)}`);
 let encoder = encoderList[parseInt(prompt('Choose encoder: ')) - 1] || '';
 // gogogo
 (async function () {
     console.log(`inputDir : ${inputDir}`)
     console.log(`outputDir: ${outputDir}`)
-    console.log(`decoder  : ${decoder}`)
+    //console.log(`decoder  : ${decoder}`)
     console.log(`encoder  : ${encoder}`)
 
     let parseVideo = filename => new Promise((resolve, reject) => {
@@ -29,10 +31,7 @@ let encoder = encoderList[parseInt(prompt('Choose encoder: ')) - 1] || '';
             .videoCodec(encoder)
             .audioCodec('copy')
             .on('start', commandLine => {
-                console.log('Spawned Ffmpeg with command: ' + commandLine);
-            })
-            .on('progress', progress => {
-                console.log('Processing: ' + progress.percent + '% done');
+                console.log('Spawned FFmpeg with command: \n' + commandLine);
             })
             .on('error', (err) => {
                 console.error(`[ffmpeg] error: \n${err.message}`);
