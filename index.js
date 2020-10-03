e05ec7e39d69f337fdc75915e0014f7e7722dbf5
gnehs <jayuiop@gmail.com>
2020-10-03 11:42:03 +0800

    nv decode, preset slow

--- index.js
+++ index.js
@@ -12,25 +12,30 @@ if (!fs.existsSync(outputDir)) {
 // encoder & decoder
 let list2console = list => list.map((x, i) => (`${i + 1}. ${x}`)).join('\n')
 let encoderList = ['hevc_nvenc', 'h264_nvenc']
-/*
 let decoderList = ['h264_cuvid', 'hevc_cuvid', 'mpeg4_cuvid', 'vc1_cuvid', 'vp8_cuvid', 'vp9_cuvid', 'h263_cuvid', 'mjpeg_cuvid', 'mpeg1_cuvid', 'mpeg2_cuvid', 'cpu']
+
 console.log(`===\nDecoder list:\n${list2console(decoderList)}`);
 let decoder = decoderList[parseInt(prompt('Choose decoder: ')) - 1] || '';
-*/
+
 console.log(`===\nEncoder list:\n${list2console(encoderList)}`);
 let encoder = encoderList[parseInt(prompt('Choose encoder: ')) - 1] || '';
 // gogogo
 (async function () {
     console.log(`inputDir : ${inputDir}`)
     console.log(`outputDir: ${outputDir}`)
-    //console.log(`decoder  : ${decoder}`)
+    console.log(`decoder  : ${decoder}`)
     console.log(`encoder  : ${encoder}`)
 
     let parseVideo = filename => new Promise((resolve, reject) => {
         let outputFilePath = replaceExt(path.resolve(outputDir, filename), '.mp4');
+        // inputOptions
+        let inputOptions = []
+        if (decoder != 'cpu') inputOptions.push('-hwaccel cuvid', `-c:v ${decoder}`)
         // check file exists
         if (!fs.existsSync(outputFilePath)) {
             ffmpeg(path.resolve(inputDir, filename))
+                .inputOptions(inputOptions)
+                .outputOptions(['-profile:v main10', '-preset slow'])
                 .videoCodec(encoder)
                 .audioCodec('copy')
                 .on('start', commandLine => {
